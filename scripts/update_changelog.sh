95a4ad2406393081fdc54ae53f1b7c7784f59a25
Michał Zegan <webczat@outlook.com>
2025-12-29 21:23:00 +0000

    Add support for shell scripts and script to update changelog on release

---
+++ scripts/update_changelog.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+# This script updates the changelog by replacing unreleased release notes with versioned ones,
+# marking them with current date as release date.
+# It also re-adds the empty unreleased block.
+
+if [[ $# -ne 1 ]]; then
+    echo Needs the release version
+    exit 1
+fi
+
+VERSION="$1"
+
+if [[ ! -f CHANGELOG.md ]]; then
+    echo The changelog does not exist
+fi
+
+DATE="$(date "+%Y-%m-%d")"
+
+sed -i -r -e "s/^##\s*\[Unreleased\]/## [Unreleased]\n\n## [$VERSION] $DATE/" CHANGELOG.md
